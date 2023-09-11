34b97954bca461a1914a154829f6a88edda3819c
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-09-11 21:00:27 +0100

    adding conversation link and server

---
+++ src/app/(main)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface pageProps {}
+
+const page = ({}: pageProps) => {
+  return <div>Channel Id</div>
+}
+
+export default page
--- src/app/(main)/servers/[serverId]/page.tsx
+++ src/app/(main)/servers/[serverId]/page.tsx
@@ -1,11 +1,35 @@
+import currentProfile from '@/lib/currentProfile'
+import { redirectToSignIn } from '@clerk/nextjs'
 import React from 'react'
+import { prisma as db } from '@/lib/db'
+import { redirect } from 'next/navigation'
 
 interface ServerIdPageProps {
-  
+  params: { serverId: string }
 }
 
-const ServerIdPage = ({}: ServerIdPageProps) => {
-  return <div>ServerIdPage</div>
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile()
+  if (!profile) {
+    return redirectToSignIn()
+  }
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: { some: { profileId: profile.id } },
+    },
+    include: {
+      channels: { where: { name: 'general' }, orderBy: { createdAt: 'asc' } },
+    },
+  })
+
+  const generalChannel = server?.channels[0]
+
+  if (generalChannel?.name !== 'general') {
+    return null
+  }
+  return redirect(`/servers/${params.serverId}/channels/${generalChannel.id}`)
+
 }
 
 export default ServerIdPage
