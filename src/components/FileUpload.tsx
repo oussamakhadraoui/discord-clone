8882928b55135d72a2cc3171ff18ac1d45fb8d6f
galipo1996 <105125889+galipo1996@users.noreply.github.com>
2023-09-12 23:57:48 +0100

    file upload modified

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { UploadDropzone } from '../utils/uploadthing'
 import '@uploadthing/react/styles.css'
-import { X } from 'lucide-react'
+import { FileIcon, X } from 'lucide-react'
 import Image from 'next/image'
 interface FileUploadProps {
   endpoint: 'serverImage' | 'messageFile'
@@ -11,20 +11,45 @@ interface FileUploadProps {
 
 const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
   const fileType = value?.split('.').pop()
+
   if (value && fileType !== 'pdf') {
     return (
       <div className='relative h-20 w-20'>
-        <Image fill src={value} alt='upload' className='rounded-full' />
+        <Image fill src={value} alt='Upload' className='rounded-full' />
         <button
-          className='rounded-full bg-rose-500 text-white absolute top-0 p-1 right-0 shadow-sm'
-          type='button'
           onClick={() => onChange('')}
+          className='bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm'
+          type='button'
         >
-          <X className='w-4 h-4' />
+          <X className='h-4 w-4' />
         </button>
       </div>
     )
   }
+
+  if (value && fileType === 'pdf') {
+    return (
+      <div className='relative flex items-center p-2 mt-2 rounded-md bg-background/10'>
+        <FileIcon className='h-10 w-10 fill-indigo-200 stroke-indigo-400' />
+        <a
+          href={value}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline'
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange('')}
+          className='bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm'
+          type='button'
+        >
+          <X className='h-4 w-4' />
+        </button>
+      </div>
+    )
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
